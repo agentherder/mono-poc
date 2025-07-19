8959a9e8d8b2bc3d8251019303ef03a14af3c105
Warren Whipple <w@warrenwhipple.com>
2025-07-19 19:57:36 +0100

    Simpify web config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(() => ({
   plugins: [
     tsConfigPaths({ projects: ['./tsconfig.json'] }),
     tanstackStart({
-      target: 'cloudflare-pages',
+      target: 'cloudflare-module',
       spa: { enabled: true },
       customViteReactPlugin: true,
     }),
