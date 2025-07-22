f182cef86e3e050ccfb58eba4f5acfc847e0b80b
Warren Whipple <w@warrenwhipple.com>
2025-07-22 08:08:28 +0100

    chore(extension-e2e): load extension with custom fixture

--- apps/extension-e2e/src/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-// WXT is not yet set up for E2E testing so this is just a test scaffold
-test('basic test', async ({ page }) => {
-  await page.goto('https://example.com');
-  expect(await page.title()).toContain('Example');
-});
