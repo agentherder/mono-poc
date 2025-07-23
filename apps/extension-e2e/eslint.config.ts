8bdcc661237360fe4badce68dca22aa032b9a202
Warren Whipple <w@warrenwhipple.com>
2025-07-23 23:04:02 +0200

    simplify nx boilerplate config on ts

--- apps/extension-e2e/eslint.config.ts
+++ apps/extension-e2e/eslint.config.ts
@@ -1,11 +1,11 @@
 import playwright from 'eslint-plugin-playwright';
-import baseConfig from '../../eslint.config.mjs';
+import baseConfig from '../../eslint.config.ts';
 
 export default [
   playwright.configs['flat/recommended'],
   ...baseConfig,
   {
-    files: ['**/*.ts', '**/*.js'],
+    files: ['**/*.ts'],
     // Override or add rules here
     rules: {},
   },
--- apps/web/eslint.config.ts
+++ apps/web/eslint.config.ts
@@ -1,11 +1,11 @@
 import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
+import baseConfig from '../../eslint.config.ts';
 
 export default [
   ...baseConfig,
   ...nx.configs['flat/react'],
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: ['**/*.ts'],
     // Override or add rules here
     rules: {},
   },
